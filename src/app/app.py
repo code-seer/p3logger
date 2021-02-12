f3563a5ac5bf9745d51acd442de3e0328be10b54
Biz Melesse <bizmelesse@gmail.com>
2021-02-11 18:02:47 -0600

    Decode user code from base64 before calling the logger

--- src/app/app.py
+++ src/app/app.py
@@ -1,23 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import base64
 
 from src.app.logger import pylogger
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/visualizer', methods=["POST"])
 def user_code_logger():
-    user_code = """input = 'Sideshow,Bob,1972,4,1,male'
-tokens = input.split(',')
-firstName = tokens[0]
-lastName = tokens[1]
-birthdate = (int(tokens[2]), int(tokens[3]), int(tokens[4]))
-isMale = (tokens[5] == 'male')
-fullName = firstName + ' ' + lastName
-
-print('Hi ' + fullName)
-    """
-    return jsonify(pylogger.run_logger(user_code))
-
-#     return 'Hello, World!'
-#
-# user_code_logger()
+    try:
+        body = request.json
+        py_version = body.get("python_version")
+        user_code = base64.b64decode(body.get("user_code"))
+        return jsonify(pylogger.run_logger(user_code))
+    except AttributeError:
+        return jsonify({
+            "error": "Please provide a body with all the required fields"
+        })
+    except Exception as e:
+        return jsonify(e.message), 500
